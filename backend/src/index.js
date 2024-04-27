b3bcbe36c58bc2e40eb7c8444bc693aeae6643cb
khushboo-choudhary <30701525+khushboo-choudhary@users.noreply.github.com>
2024-04-26 21:43:32 -0400

    Api route created.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const connect = require("./configs/db");
+require("dotenv").config();
 
 const app = express();
+const dashboardApi = require("./controllers/dashboardController");
+
 app.use(express.json());
+
+// connect the dashboard collection
+app.use("/", dashboardApi);
 
 // server connection
 app.listen(2345, async () => {
   try {
     await connect();
-    console.log("Server Connected Successfully!");
+    console.log("Server Connected Successfully!,2345");
   } catch {
     console.error("Failed to Connect!");
   }
