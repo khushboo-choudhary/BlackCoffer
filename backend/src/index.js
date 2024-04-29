ff93f59c8c7240d12be78b17672476b15fc57362
khushboo-choudhary <30701525+khushboo-choudhary@users.noreply.github.com>
2024-04-28 23:46:06 -0400

    cors implemented.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const connect = require("./configs/db");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 const dashboardApi = require("./controllers/dashboardController");
 
+app.use(cors());
 app.use(express.json());
 
 // connect the dashboard collection
