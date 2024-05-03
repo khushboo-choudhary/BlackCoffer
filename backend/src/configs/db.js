4416a540a2ea511a84c31c11494dd32687a5582f
khushboo-choudhary <30701525+khushboo-choudhary@users.noreply.github.com>
2024-05-03 02:30:53 -0400

    url changed.

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -2,5 +2,8 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 module.exports = () => {
-  return mongoose.connect(process.env.MONGODB_URL);
+  return mongoose.connect(
+    "mongodb+srv://kkhushboo321:SNPH4ovhHM3aJcby@cluster0.clm23bx.mongodb.net/dashboard?"
+  );
+  // return mongoose.connect(process.env.MONGODB_URL);
 };
